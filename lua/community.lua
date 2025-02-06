7ca5c4b873d42396e537d4ae069b5ac94d3393d4
Valentin Grégoire <valentin.gregoire@gmail.com>
2025-02-06 13:16:58 +0100

    added some common language packs

--- lua/community.lua
+++ lua/community.lua
@@ -7,19 +7,34 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- language packs
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  { import = "astrocommunity.bars-and-lines.statuscol-nvim" },
-  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
-  { import = "astrocommunity.test.neotest" },
-  { import = "astrocommunity.test.nvim-coverage" },
-  -- { import = "astrocommunity.colorscheme." },
-  -- { import = "astrocommunity." },
-  -- { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.media.image-nvim" },
-  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- debugging
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
   { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+
+  -- testing
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+
+  -- markdown
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  -- UI
+  { import = "astrocommunity.media.image-nvim" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.bars-and-lines.statuscol-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.utility.hover-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
 }
